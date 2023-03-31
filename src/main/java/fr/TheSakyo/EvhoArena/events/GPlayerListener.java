590ffe89301cde5af1a9d540a3ab63d5a08ae8c0
TheSakyo <sacha.cardone@gmail.com>
2023-03-31 21:02:35 +0200

    Initial commit

--- src/main/java/fr/TheSakyo/EvhoArena/events/GPlayerListener.java
+++ src/main/java/fr/TheSakyo/EvhoArena/events/GPlayerListener.java
@@ -9,22 +9,18 @@ import fr.TheSakyo.EvhoUtility.managers.ScoreboardManager;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockFormEvent;
-import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.*;
 
 import fr.TheSakyo.EvhoArena.ArenaMain;
 import fr.TheSakyo.EvhoArena.enums.GState;
 import fr.TheSakyo.EvhoArena.task.GWaiting;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
@@ -34,9 +30,9 @@ public class GPlayerListener implements Listener {
 
 	/* Récupère la class "Main" */
 	private ArenaMain main;
+
 	public GPlayerListener(ArenaMain pluginMain) { this.main = pluginMain; }
 	/* Récupère la class "Main" */
-
 
 
 	// "Location" du lobby //
@@ -44,35 +40,33 @@ public class GPlayerListener implements Listener {
 	// "Location" du lobby //
 
 
-
-	/*********************/
-	/* PARTIE CONNECTION */
-	/*********************/
-
+	/***********************************/
+	/* PARTIE CONNECTION/DÉCONNECTION */
+	/**********************************/
 
 	// Évènement quand un joueur rejoint le serveur //
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
-	
+
 		Player p = e.getPlayer();
-		
+
 		if(main.playerKills.get(p.getUniqueId().toString()) == null) { main.playerKills.put(p.getUniqueId().toString(), 0); }
 
 		if(main.Kills.get(p.getUniqueId()) == null) main.Kills.put(p.getUniqueId(), 0);
 
 		//Si le joueur rejoint le serveur pendant que la partie est en train de se terminer
 		//Il est donc téléporter au serveur "Hub"
-		if(main.manager.isState(GState.FINISH)) {	
-			
+		if(main.manager.isState(GState.FINISH)) {
+
 			main.manager.ConnectPlayerToHubServer(p);
 			return;
 		}
 
 		//Essaye de vérifier si le spawn lobby est définit pour continuer le code
 		try {
-			
+
 			World World = Bukkit.getServer().getWorld(ConfigFile.getString(main.config, "lobby.World"));
-			
+
 			Double X = ConfigFile.getDouble(main.config, "lobby.X");
 			Double Y = ConfigFile.getDouble(main.config, "lobby.Y");
 			Double Z = ConfigFile.getDouble(main.config, "lobby.Z");
@@ -81,48 +75,46 @@ public class GPlayerListener implements Listener {
 
 			//Essaye de définit une "Location" du spawn lobby (vérifie si les spawn définit sont bien des coordonnées)
 			try {
-				
+
 				loclobby = new Location(World, X, Y, Z, Yaw, Pitch);
-				
+
 				p.teleport(loclobby);
 				p.getInventory().clear();
 				p.setFoodLevel(20);
 				p.setHealth(p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue());
-				
+
 				p.sendMessage(main.prefix + ChatColor.AQUA + "/hub ou /lobby pour retourner au hub !");
-				
+
 				p.sendMessage(main.prefix + ChatColor.AQUA + "PvPArena chacun pour soi ! Créez vous des alliances et/ou trahisez vous !");
-				
+
 				if(!main.manager.isState(GState.WAITING)) {
-					
+
 					p.setGameMode(GameMode.SPECTATOR);
-					
-					if(main.manager.isState(GState.STARTING)) { p.sendMessage(main.prefix + ChatColor.GRAY.toString() + ChatColor.ITALIC.toString() + "Le jeu est en cours de démarrage ! ");  }
-						
-					else { p.sendMessage(main.prefix + ChatColor.GRAY.toString() + ChatColor.ITALIC.toString() + "Le jeu a déjà démarré ! "); }
-					
+
+					if(main.manager.isState(GState.STARTING)) { p.sendMessage(main.prefix + ChatColor.GRAY.toString() + ChatColor.ITALIC.toString() + "Le jeu est en cours de démarrage ! ");  } else { p.sendMessage(main.prefix + ChatColor.GRAY.toString() + ChatColor.ITALIC.toString() + "Le jeu a déjà démarré ! "); }
+
 					return;
 				}
-				
+
 				if(!main.manager.getPlayers().contains(p)) main.manager.getPlayers().add(p);
-				
+
 				p.setGameMode(GameMode.ADVENTURE);
 				p.setLevel(0);
 				p.setExp(0);
 				GInventoryListener.setItemLobby(p);
-				
+
 				if(!main.manager.getPlayers().isEmpty() && main.manager.getPlayers().size() <= 1) { new GWaiting(main).runTaskTimer(main, 0, 20L); }
 
-			//Un message d'erreur est envoyé si le spawn lobby est pas définit
-			} catch(NullPointerException ex) { 
-				
+				//Un message d'erreur est envoyé si le spawn lobby est pas définit
+			} catch(NullPointerException ex) {
+
 				String error = ChatColor.RED + "Point de spawn lobby non éxistant ! \n Si vous n'êtes pas administrateur, veuillez demander à un administrateur de le définir !";
-				
+
 				p.sendMessage(main.prefix + error);
-			
+
 			}
 
-  		//Un message d'erreur est envoyé si le spawn lobby est pas définit et qu'il y'a un erreur dans la configuration (Coordonnées du spawn null)"
+			//Un message d'erreur est envoyé si le spawn lobby est pas définit et qu'il y'a un erreur dans la configuration (Coordonnées du spawn null)"
 		} catch(IllegalArgumentException ex) {
 
 			String error = ChatColor.RED + "Point de spawn lobby non éxistant, Erreur de Configuration ! \n Si vous n'êtes pas administrateur, veuillez demander à un administrateur de le définir !";
@@ -148,7 +140,7 @@ public class GPlayerListener implements Listener {
 	// Évènement quand un joueur quitte le serveur //
 	@EventHandler
 	public void onQuit(PlayerQuitEvent e) {
-		
+
 		Player p = e.getPlayer();
 
 		// Retire les information du scoreboard au joueur //
@@ -163,13 +155,20 @@ public class GPlayerListener implements Listener {
 		//Retire le joueur de la liste des joueurs en jeu
 		if(main.manager.getPlayers().contains(p)) main.manager.getPlayers().remove(p);
 
-		
-		if(main.manager.isState(GState.WAITING) || main.manager.isState(GState.STARTING) || main.manager.isState(GState.FINISH)) { return; }
-		else { main.manager.CheckWin(p); } //Vérifie qui à gagner s'il y'a un gagnant si le joueur quitte en pleine jeu
+
+		if(main.manager.isState(GState.WAITING) || main.manager.isState(GState.STARTING) || main.manager.isState(GState.FINISH)) { return; } else { main.manager.CheckWin(p); } //Vérifie qui à gagner s'il y'a un gagnant si le joueur quitte en pleine jeu
 
 	}
 	// Évènement quand un joueur rejoint le serveur //
 
+	/***********************************/
+	/* PARTIE CONNECTION/DÉCONNECTION */
+	/**********************************/
+
+
+	/**************************************/
+	/* PARTIE INTÉRACTION AVEC LE JOUEUR */
+	/*************************************/
 
 	// Évènement quand le joueur jette un item //
 	@EventHandler
@@ -177,8 +176,7 @@ public class GPlayerListener implements Listener {
 
 		Player p = e.getPlayer();
 
-		if(fr.TheSakyo.EvhoUtility.utils.custom.CustomMethod.hasByPassPerm(p)) { e.setCancelled(false); }
-		else { e.setCancelled(true); }
+		if(fr.TheSakyo.EvhoUtility.utils.custom.CustomMethod.hasByPassPerm(p)) { e.setCancelled(false); } else { e.setCancelled(true); }
 	}
 	// Évènement quand le joueur jette un item //
 
@@ -196,15 +194,13 @@ public class GPlayerListener implements Listener {
 	// Évènement quand le joueur rammasse un item //
 
 
-
 	// Évènement quand le joueur casse un bloc //
 	@EventHandler
 	public void onBreak(BlockBreakEvent e) {
 
 		Player p = e.getPlayer();
 
-		if(fr.TheSakyo.EvhoUtility.utils.custom.CustomMethod.hasByPassPerm(p)) { e.setCancelled(false); }
-		else { e.setCancelled(true); }
+		if(fr.TheSakyo.EvhoUtility.utils.custom.CustomMethod.hasByPassPerm(p)) { e.setCancelled(false); } else { e.setCancelled(true); }
 	}
 	// Évènement quand le joueur casse un bloc //
 
@@ -214,47 +210,61 @@ public class GPlayerListener implements Listener {
 	public void onPlace(BlockPlaceEvent e) {
 
 		Player p = e.getPlayer();
-
-		ItemStack it = e.getItemInHand();
-
-		Block placedBlock = e.getBlockPlaced();
-		Block blockAgainst = e.getBlockAgainst();
+		Block block = e.getBlock();
+		Material type = e.getBlock().getType();
 
 		if(fr.TheSakyo.EvhoUtility.utils.custom.CustomMethod.hasByPassPerm(p)) { e.setCancelled(false); }
-		else e.setCancelled(true);
+		else { e.setCancelled(true); }
 	}
 	// Évènement quand le joueur pose un bloc //
 
-						/* ---------------------------------------------------------------------- */
+			/* ---------------------------------------------------------------------- */
 
-	// Évènement quand un bloc apparaît automatiquement suite à un autre évènement //
+	// Évènement quand le joueur vide un seau (lave ou eau) //
 	@EventHandler
-	public void onFromTo(BlockFromToEvent e) {
+	public void onPlaceLiquid(PlayerBucketEmptyEvent e) {
 
-		e.setCancelled(true);
+		Player p = e.getPlayer(); // Récupère le joueur qui a effectuer l'action
+		Material bucket = e.getBucket(); // Vérifie le seau en question
 
-								/* ------------------------------------------- */
+		Block block = e.getBlock(); // Récupère le bloc dans le seau qui a été placer
+		Block blockClicked = e.getBlockClicked(); // Récupère bloc qui a été cliuer
 
-		// Aprés cinq secondes, on recharge les 'NPCs' & hologrammes pour le Joueur //
-		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(main, new Runnable() {
+		// Si le joueur n'a pas la permission, on effectue quelque permissions une fois le liquide placer
+		if(!fr.TheSakyo.EvhoUtility.utils.custom.CustomMethod.hasByPassPerm(p)) {
 
-			@Override
-			public void run() { e.getBlock().setType(Material.AIR); }
+			/* ⬇️ Si le seau en question est un 'seau d'eau' ou un 'seau de lave' alors on annule l'évènement, on définit un bloc liquide en fonction du seau récupérer,
+			   ensuite on met à jour son état de force pour empêcher de le faire couler, aprés 5 seconde on retire le bloc ⬇️ */
+			if(bucket == Material.WATER_BUCKET || bucket == Material.LAVA_BUCKET) {
 
-		}, 20*5);
-		// Aprés cinq secondes, on recharge les 'NPCs' & hologrammes pour le Joueur //
+				e.setCancelled(true); // Annule l'évènement
+
+									/* ----------------------------------------- */
+
+				// On créer une donnée de bloc pour un liquide comme de l'eau ou de lave en fonction du seau qui a été récupérer
+				BlockData blockData = Bukkit.getServer().createBlockData(bucket == Material.WATER_BUCKET ? Material.WATER : Material.LAVA);
+
+				Block blockAbove = blockClicked.getRelative(e.getBlockFace()); // On récupère le bloc au-dessus du bloc qui a été cliquer
+				blockAbove.setBlockData(blockData, false); // On définit une donnée de bloc au bloc au-dessus du bloc qui a été cliquer en précisant qu'on veut aucune propagation
+
+									/* ----------------------------------------- */
+
+				// Aprés trois secondes, on remplace le fameux bloc liquide par de l'air (retire le bloc) //
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(main, new Runnable() {
+
+					@Override
+					public void run() { blockAbove.setType(Material.AIR); }
+
+				}, 20*3);
+				// Aprés trois secondes, on remplace le fameux bloc liquide par de l'air (retire le bloc) //
+			}
+			/* ⬆️ Si le seau en question est un 'seau d'eau' ou un 'seau de lave' alors on annule l'évènement, on définit un bloc liquide en fonction du seau récupérer,
+			   ensuite on met à jour son état de force pour empêcher de le faire couler, aprés 5 seconde on retire le bloc ⬆️ */
+		}
 	}
-	// Évènement quand un bloc apparaît automatiquement suite à un autre évènement //
+	// Évènement quand le joueur vide un seau (lave ou eau) //
 
-										/* ------------------------------------ */
-
-	// Évènement quand un bloc se forme automatiquement //
-	@EventHandler
-	public void onFromTo(BlockFormEvent e) { e.setCancelled(true); }
-	// Évènement quand un bloc se forme automatiquement //
-
-	/*********************/
-	/* PARTIE CONNECTION */
-	/*********************/
-	
+	/**************************************/
+	/* PARTIE INTÉRACTION AVEC LE JOUEUR */
+	/*************************************/
 }
