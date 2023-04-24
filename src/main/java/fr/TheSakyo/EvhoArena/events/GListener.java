2917f81af4c34874183bece8e121d599cd7fdd10
TheSakyo <sacha.cardone@gmail.com>
2023-04-24 19:44:15 +0200

    Initial commit

---
+++ src/main/java/fr/TheSakyo/EvhoArena/events/GListener.java
@@ -0,0 +1,28 @@
+package fr.TheSakyo.EvhoArena.events;
+
+import fr.TheSakyo.EvhoArena.ArenaMain;
+import org.bukkit.GameRule;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.world.WorldLoadEvent;
+
+public class GListener implements Listener {
+
+    /* Récupère la class "Main" */
+	private ArenaMain main;
+
+	public GListener(ArenaMain pluginMain) { this.main = pluginMain; }
+	/* Récupère la class "Main" */
+
+	/*****************************/
+	/* PARTIE ÉVÈNEMENTS UTILES */
+	/****************************/
+
+	@EventHandler
+	public void onWorldLoad(WorldLoadEvent e) { e.getWorld().setGameRule(GameRule.DO_MOB_SPAWNING, false); }
+
+    /*****************************/
+	/* PARTIE ÉVÈNEMENTS UTILES */
+	/****************************/
+}
--- src/main/java/fr/TheSakyo/EvhoArena/registering/RegisterEvents.java
+++ src/main/java/fr/TheSakyo/EvhoArena/registering/RegisterEvents.java
@@ -3,6 +3,7 @@ package fr.TheSakyo.EvhoArena.registering;
 import fr.TheSakyo.EvhoArena.ArenaMain;
 import fr.TheSakyo.EvhoArena.events.GDamageListener;
 import fr.TheSakyo.EvhoArena.events.GInventoryListener;
+import fr.TheSakyo.EvhoArena.events.GListener;
 import fr.TheSakyo.EvhoArena.events.GPlayerListener;
 import org.bukkit.plugin.PluginManager;
 
@@ -10,7 +11,10 @@ public class RegisterEvents {
 
     public static void init(ArenaMain main, PluginManager pluginManager) {
 
-		   pluginManager.registerEvents(new GPlayerListener(main), main); //Création des évenements du joueurs
-		   pluginManager.registerEvents(new GInventoryListener(main), main); //Création des évenements du joueurs
+        pluginManager.registerEvents(new GPlayerListener(main), main); //Création des évenements du joueurs
+        pluginManager.registerEvents(new GDamageListener(main), main); //Création des évenements des dommages
+        pluginManager.registerEvents(new GInventoryListener(main), main); //Création des évenements du joueurs
+
+        pluginManager.registerEvents(new GListener(main), main); //Création des évenements utiles
     }
 }
