6530de8f20142ef3e60e5edd78b3b8da4e01c5d4
TheSakyo <sacha.cardone@gmail.com>
2024-06-27 11:59:13 +0200

    Initial commit

--- src/main/java/fr/TheSakyo/EvhoArena/registering/RegisterCommands.java
+++ src/main/java/fr/TheSakyo/EvhoArena/registering/RegisterCommands.java
@@ -15,7 +15,7 @@ public class RegisterCommands {
 		   main.getCommand("enablegame").setExecutor((CommandExecutor)new EnableGameCommand(main)); //Commande pour activer le mini-jeux
 		   main.getCommand("setlobby").setExecutor((CommandExecutor)new SetLobbyCommand(main)); //Commande pour la création du point de spawn lobby
 		   main.getCommand("setzonegame").setExecutor((CommandExecutor)new SetZoneGameCommand(main)); //Commande pour la création de la zone de jeu de l'arêne
-		   /*main.getCommand("setgame").setExecutor((CommandExecutor)new SetGameCommand(main)); //Commande pour la création du point de spawn de l'arêne*/
+		   main.getCommand("setgame").setExecutor((CommandExecutor)new SetGameCommand(main)); //Commande pour la création du point de spawn de l'arêne
 
 		   main.getCommand("listlobby").setExecutor((CommandExecutor)new ListLobbyCommand(main)); //Commande pour afficher la liste des points de spawn lobby
 		   main.getCommand("listgame").setExecutor((CommandExecutor)new ListGameCommand(main)); //Commande pour afficher la liste des points de spawn lobby
@@ -23,7 +23,7 @@ public class RegisterCommands {
 		   main.getCommand("disablegame").setExecutor((CommandExecutor)new DisableGameCommand(main)); //Commande pour désactiver le mini-jeux
 		   main.getCommand("unsetlobby").setExecutor((CommandExecutor)new UnSetLobbyCommand(main)); //Commande pour l'annulation du point de spawn lobby
 		   main.getCommand("unsetzonegame").setExecutor((CommandExecutor)new UnSetZoneGameCommand(main)); //Commande pour l'annulation de la zone de jeu de l'arêne
-		   /*main.getCommand("unsetgame").setExecutor((CommandExecutor)new UnSetGameCommand(main)); //Commande pour l'annulation du point de spawn de l'arêne*/
+		   main.getCommand("unsetgame").setExecutor((CommandExecutor)new UnSetGameCommand(main)); //Commande pour l'annulation du point de spawn de l'arêne
 
     }
 }
